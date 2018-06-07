254a32e1f48cefa08e1b65fddf16d5c9b865bcb1
doyewo <dexteroyewo@gmail.com>
2018-06-06 23:44:33 -0500

    part 2

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -4,11 +4,26 @@ import java.util.Arrays;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
-  }
+    public static void main(final String[] args) {
+        int max = 0;
+        if (args.length > 0) {
+            try {
+                max = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.err.println("The argument entered must be an integer ");
+                //In our case, you can use System.out.println.
+                System.exit(1);
+            }
+            /* Since you are trapping the error, you can exit with a 0 instead of
+             * 1.  By convention, a 0 means a normal exit (no error), and a non-
+             * zero value means some type of error.  But it still ends the program
+             * and returns to the OS.  So it’s more of a status message to you.
+             */
+        }
+        if (Integer.parseInt(args[0]) < 0) {
+            System.out.println("Must be a positive integer. Please try again");
+        }
+
+        FizzBuzz.fizzBuzz(max);
+    }
 }
